d28803257a52039af45a839bae1b81bb32927be0
Marco <marco.wettstein@gmail.com>
2021-07-01 10:41:31 +0200

    Disable POST on Motorcycles Function

--- api/Motorcycles.cs
+++ api/Motorcycles.cs
@@ -22,7 +22,7 @@ namespace ch.wettsti
 
         [FunctionName("motorcycles")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
